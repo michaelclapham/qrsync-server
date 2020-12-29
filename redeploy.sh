bab69522fcae8f1af2d6c145749483f34c0b4743
Michael Clapham <michaeljclapham@gmail.com>
2020-12-29 20:10:09 +0000

    Make docker container restart

--- redeploy.sh
+++ redeploy.sh
@@ -3,4 +3,4 @@ git pull
 docker build -t qrsync_server .
 docker stop qrsync_server || true
 docker system prune -f
-docker run -d -p 4001:4001 -it qrsync_server
+docker run -d -p 4001:4001 -it qrsync_server --restart=unless-stopped
