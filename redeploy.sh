b7e86f8bcf8bc24b538f7f7ea5c1636d0aaf9ff6
Michael Clapham <michaeljclapham@gmail.com>
2020-12-29 23:28:49 +0000

    Change port to 4010

--- redeploy.sh
+++ redeploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+git reset --hard
 git pull
 chmod a+x ./redeploy.sh
 docker build -t qrsync_server .
 docker rm -f qrsync_server || true
 docker system prune -f
 fuser -k 4010/tcp
-docker run -d -p 4001:4001 -it qrsync_server --restart=unless-stopped
+docker run -d -p 4010:4010 -it qrsync_server --restart=unless-stopped
