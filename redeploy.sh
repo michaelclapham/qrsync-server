f4505ec53ec1b72f75e936b795c2ce5a91cfaa7d
Michael Clapham <michaeljclapham@gmail.com>
2020-12-30 17:12:19 +0000

    Fix redeploy script

--- redeploy.sh
+++ redeploy.sh
@@ -3,7 +3,7 @@ git reset --hard
 git pull
 chmod a+x ./redeploy.sh
 docker build -t qrsync_server .
-EXISTING_CONTAINER_ID=docker container ls --format "table {{.ID}}\t{{.Ports}}" -a | grep "4010->4010" | awk '{print $1}'
+EXISTING_CONTAINER_ID=$(docker container ls --format "table {{.ID}}\t{{.Ports}}" -a | grep "4010->4010" | awk '{print $1}')
 if [ -z EXISTING_CONTAINER_ID ]
 then
     docker stop $EXISTING_CONTAINER_ID || true
