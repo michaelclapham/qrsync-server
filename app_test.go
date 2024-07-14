4435a13e155f07e722c1824da37a052f4b4bf4db
Michael Clapham <michaelclapham@users.noreply.github.com>
2024-07-14 20:17:41 +0100

    Add test for making sure 2 clients connect with unique id

--- app_test.go
+++ app_test.go
@@ -27,40 +27,43 @@ func TestServerStarts(t *testing.T) {
 	log.Printf("%s", clientsUrl)
 }
 
-func SetupServerAndConnect(t *testing.T) (*websocket.Conn, *httptest.Server) {
+func SetupWsServer(t *testing.T) (*httptest.Server, string) {
 	app := App{}
 	app.Init()
 	testServer := httptest.NewServer(app.MainHandler())
 
 	// Convert http://127.0.0.1 to ws://127.0.0/api/v1/ws
-	u := "ws" + strings.TrimPrefix(testServer.URL, "http") + "/api/v1/ws"
+	wsUrl := "ws" + strings.TrimPrefix(testServer.URL, "http") + "/api/v1/ws"
 
-	log.Printf("Url %s", u)
-
-	// Connect to the server
-	ws, _, err := websocket.DefaultDialer.Dial(u, nil)
-	if err != nil {
-		t.Fatalf("%v", err)
-	}
-	return ws, testServer
+	return testServer, wsUrl
 }
 
 func TestServerStartsAndWebsocketCanConnect(t *testing.T) {
-	ws, testServer := SetupServerAndConnect(t)
-	defer ws.Close()
+	testServer, wsUrl := SetupWsServer(t)
 	defer testServer.Close()
 
+	ws, _, err := websocket.DefaultDialer.Dial(wsUrl, nil)
+	if err != nil {
+		t.Fatalf("Failed to connect to websocket server: %v", err)
+	}
+	defer ws.Close()
+
 	// Send message to server, read response and check to see if it's what we expect.
-	_, _, err := ws.ReadMessage()
+	_, _, err = ws.ReadMessage()
 	if err != nil {
 		t.Fatalf("%v", err)
 	}
 }
 
 func TestFirstMessageIsClientConnect(t *testing.T) {
-	ws, testServer := SetupServerAndConnect(t)
-	defer ws.Close()
+	testServer, wsUrl := SetupWsServer(t)
 	defer testServer.Close()
+
+	ws, _, err := websocket.DefaultDialer.Dial(wsUrl, nil)
+	if err != nil {
+		t.Fatalf("Failed to connect to websocket server: %v", err)
+	}
+	defer ws.Close()
 
 	_, msgBytes, err := ws.ReadMessage()
 	if err != nil {
@@ -71,5 +74,72 @@ func TestFirstMessageIsClientConnect(t *testing.T) {
 	json.Unmarshal(msgBytes, &connectMsgJson)
 	if connectMsgJson["type"] != "ClientConnect" {
 		t.Fatalf("Expected type to be ClientConnect but was %s", connectMsgJson["type"])
+	}
+}
+
+func TestFirstMessageHasClient(t *testing.T) {
+	testServer, wsUrl := SetupWsServer(t)
+	defer testServer.Close()
+
+	ws, _, err := websocket.DefaultDialer.Dial(wsUrl, nil)
+	if err != nil {
+		t.Fatalf("Failed to connect to websocket server: %v", err)
+	}
+	defer ws.Close()
+
+	_, msgBytes, err := ws.ReadMessage()
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	var clientConnectMsg ClientConnectMsg
+	err = json.Unmarshal(msgBytes, &clientConnectMsg)
+	if err != nil {
+		t.Fatalf("Error parsing message json as ClientConnectMsg: %v", err)
+	}
+}
+
+func TestTwoClientsCanConnectAndHaveDifferentIds(t *testing.T) {
+	testServer, wsUrl := SetupWsServer(t)
+	defer testServer.Close()
+
+	// Client 1 connect
+	ws, _, err := websocket.DefaultDialer.Dial(wsUrl, nil)
+	if err != nil {
+		t.Fatalf("Failed to connect to websocket server: %v", err)
+	}
+	defer ws.Close()
+
+	_, msgBytes, err := ws.ReadMessage()
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	var client1ConnectMsg ClientConnectMsg
+	err = json.Unmarshal(msgBytes, &client1ConnectMsg)
+	if err != nil {
+		t.Fatalf("Error parsing client1 message json as ClientConnectMsg: %v", err)
+	}
+
+	// Client 2 connect
+	ws2, _, err := websocket.DefaultDialer.Dial(wsUrl, nil)
+	if err != nil {
+		t.Fatalf("Failed to connect client 1 to websocket server: %v", err)
+	}
+	defer ws2.Close()
+
+	_, msgBytes, err = ws2.ReadMessage()
+	if err != nil {
+		t.Fatalf("Failed to connect client 2 to websocket server: %v", err)
+	}
+
+	var client2ConnectMsg ClientConnectMsg
+	err = json.Unmarshal(msgBytes, &client2ConnectMsg)
+	if err != nil {
+		t.Fatalf("Error parsing client2 message json as ClientConnectMsg: %v", err)
+	}
+
+	if client1ConnectMsg.Client.ID == client2ConnectMsg.Client.ID {
+		t.Fatalf("Expected clients to each have unique id but both were %s", client2ConnectMsg.Client.ID)
 	}
 }
